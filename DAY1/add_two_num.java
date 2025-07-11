6236fc4a60ac356f8ce0e5a4d660f474017e7441
VIVEK KUMAR <VIVEK252575@gmail.com>
2025-07-11 11:49:23 +0530

    Add program to sum two numbers in Java
    Introduces add_two_num.java, a simple Java program that initializes two integers, prints them, and outputs their sum. This serves as a basic example for addition in Java.

---
+++ DAY1/add_two_num.java
@@ -0,0 +1,13 @@
+package DAY1;
+
+public class add_two_num {
+    public static void main (String[]args){
+        int a = 45; 
+        System.out.println(a);
+        int b = 55 ;
+        System.out.println(b);
+        int  sum = a+b ;
+        System.out.println(sum);
+        
+    }
+}
