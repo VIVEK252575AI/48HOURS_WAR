2206c0d14be41a5faba1acaac88f182263822123
VIVEK KUMAR <VIVEK252575@gmail.com>
2025-07-15 01:30:29 +0530

    Create sumof_n_num.java

---
+++ DAY2/sumof_n_num.java
@@ -0,0 +1,24 @@
+package DAY2;
+
+import java.util.*;
+
+public class sumof_n_num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0 ;
+        int i =1 ;
+
+        while (i <= n ) {
+            sum = sum +i ;
+            i++ ;
+         
+            
+            
+        }
+
+        System.out.println(sum);
+    }
+    
+}
